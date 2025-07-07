f5778b550f4f2fecf867a8179c7e5409524326e3
furkan <furkanktnci@gmail.com>
2025-07-07 20:33:21 +0300

    implemented CRUD operations for product model

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -14,5 +14,57 @@ const getAllProducts = async (req, res) => {
     }
 }
 
-export { getAllProducts }
+const getOneProduct = async (req, res) => {
+    try {
+        const getAProduct = await Product.findById(req.params.id);
+
+        res.status(200).json({
+            getAProduct
+        })
+    } catch (err) {
+        console.log("getOneProduct verisi çekilemedi!", err)
+    }
+}
+
+const createProduct = async (req, res) => {
+    try {
+        const createAproduct = await Product.create(req.body)
+        res.status(201).json({
+            message: "Ürün başarıyla oluşturuldu",
+            createAproduct
+        })
+    }
+    catch (err) {
+        console.log("Ürün oluştururken hata!", err)
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const deleteProduct = await Product.findById(req.params.id);
+        await deleteProduct.remove()
+
+        res.status(200).json({
+            message: "Ürün başarıyla silindi",
+            deleteProduct
+        })
+    } catch (err) {
+        console.error("Ürün silerken hata!", err)
+    }
+}
+
+const updateProduct = async (req, res) => {
+    let updateProduct = await Product.findById(req.params.id);
+
+    updateProduct = awaitProduct.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    // { new: true }: Güncellenmiş halini döndürür (aksi halde eski halini döner).
+    // { runValidators: true }: Modeldeki kurallar (örneğin boş olamaz, sayı olmalı vs.) kontrol edilir.
+
+    res.status(200).json({
+        message: "Ürün başarıyla güncellendi!",
+        updateProduct
+    })
+}
+
+export { getAllProducts, getOneProduct, createProduct, deleteProduct, updateProduct }
 
