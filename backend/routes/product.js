90534507e11d8c65765cf263b72be36425b8ca0e
furkan <furkanktnci@gmail.com>
2025-07-07 20:33:43 +0300

    routes added for product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.js";
+import { createProduct, deleteProduct, getAllProducts, getOneProduct, updateProduct } from "../controllers/product.js";
 
 const router = express.Router();
 
 router.get("/products", getAllProducts);
+router.get("/products/:id", getOneProduct);
+router.post("/product/new", createProduct);
+router.post("/product/:id", deleteProduct);
+router.post("/product/:id", updateProduct);
 
 
 export default router
