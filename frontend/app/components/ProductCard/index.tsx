aa127296fdbfaa5da9ef17627ae7d7979ab685b7
furkan <furkanktnci@gmail.com>
2025-07-14 20:31:05 +0300

    updated

--- frontend/app/components/ProductCard/index.tsx
+++ frontend/app/components/ProductCard/index.tsx
@@ -8,15 +8,15 @@ interface IProductData {
 
 const ProductCard: React.FC<IProductData> = ({ item }) => {
     return (
-        <Link href={`/detail/${item._id}`}>
+        <Link href={`products/detail/${item._id}`}>
             <div className="flex flex-col">
                 <div className="relative w-full aspect-[2/3]">
                     <Image src={item.images.url} alt={item.name} fill style={{ objectFit: 'cover' }} />
                 </div>
                 <div className="ml-2 mt-2">
-                    <div className="text-xs mb-1">{item.name}</div>
-                    <div className="text-xs mb-1 bg-[#ffeb3b] w-max px-1">{item.price} TL</div>
-                    <div className="text-xs mb-1 bg-[#000000] text-[#FFFFFF] font-medium w-max px-1">Stok:{item.stock} adet</div>
+                    <p className="text-xs mb-1">{item.name}</p>
+                    <p className="text-xs mb-1 bg-[#ffeb3b] w-max px-1">{item.price} TL</p>
+                    <p className="text-xs mb-1 bg-[#000000] text-[#FFFFFF] font-medium w-max px-1">Stok:{item.stock} adet</p>
                 </div>
             </div >
         </Link>
