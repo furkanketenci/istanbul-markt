ac29a022aa37fd2484ed559ef2ba1ca25e3e8579
furkan <furkanktnci@gmail.com>
2025-07-16 23:47:32 +0300

    detail updated

--- frontend/app/products/detail/[id]/page.tsx
+++ frontend/app/products/detail/[id]/page.tsx
@@ -5,14 +5,16 @@ import { getDetailProduct } from "@/app/redux/product/productSlice";
 import { AppDispatch, RootState } from "@/app/redux/store";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useEffect, use } from "react";
+import { useEffect, use, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const Detail = ({ params }: { params: Promise<{ id: string }> }) => {
     const { id } = use(params);
 
+    const [quantity, setQuantity] = useState(0);
     const detailProduct = useSelector((state: RootState) => state.product.detailProduct);
     const dispatch = useDispatch<AppDispatch>();
+
 
     useEffect(() => {
         if (id) {
@@ -20,9 +22,63 @@ const Detail = ({ params }: { params: Promise<{ id: string }> }) => {
         }
     }, [dispatch, id]);
 
-    console.log("detailProduct", detailProduct)
+    const increment = () => {
+        if (quantity < detailProduct.stock!) {
+            setQuantity(quantity + 1)
+        }
+    }
+
+    const decrement = () => {
+        if (quantity > 0) {
+            setQuantity(quantity - 1)
+        }
+    }
+
+    const addBasket = () => {
+        console.log("addBasket")
+    }
+
 
     if (!detailProduct.images?.url) return null;
+
+    const quantityArea = detailProduct.stock !== 0 && (
+        <>
+            <div className="flex items-center mb-4">
+                <Button
+                    onClick={increment}
+                    className="
+                        flex 
+                        items-center 
+                        justify-center 
+                        text-[rgb(38,38,38)] 
+                        text-sm 
+                        cursor-pointer
+                        rounded-none
+                        bg-transparent
+                        hover:bg-transparent
+                        w-[20px]">
+                    +
+                </Button>
+                <p className="mx-4 text-[rgb(38,38,38)] text-sm font-medium">{quantity}</p>
+                <Button
+                    onClick={decrement}
+                    className="
+                        flex 
+                        items-center 
+                        justify-center 
+                        text-[rgb(38,38,38)] 
+                        text-sm 
+                        cursor-pointer 
+                        rounded-none
+                        bg-transparent
+                        hover:bg-transparent
+                        w-[20px]">
+                    -
+                </Button>
+            </div>
+
+        </>
+    )
 
     return (
         <div className="flex items-center justify-start h-screen w-full">
@@ -31,7 +87,7 @@ const Detail = ({ params }: { params: Promise<{ id: string }> }) => {
                     <Logo />
                 </div>
                 <Image
-                    className="mx-0 w-full h-full xl:mx-16 xl:w-[33%] lg:w-[50%]"
+                    className="mx-0 w-full h-full xl:mx-16 xl:w-[33%] lg:w-[50%] object-contain"
                     src={detailProduct.images.url}
                     width={500}
                     height={500}
@@ -67,13 +123,18 @@ const Detail = ({ params }: { params: Promise<{ id: string }> }) => {
                         {detailProduct.description}
                     </p>
 
-                    <Button className="
-                        sticky
-                        bottom-0
-                        mt-auto
-                        mb-6
-                        w-full
-                        bg-[#FFFFFF]
+                    {quantityArea}
+
+                    <Button
+                        onClick={addBasket}
+                        disabled={detailProduct.stock === 0}
+                        className="
+                            sticky
+                            bottom-0
+                            mt-auto
+                            mb-6
+                            w-full
+                            bg-[#FFFFFF]
                         text-[#000000]
                         border border-black
                         rounded-none
@@ -86,7 +147,9 @@ const Detail = ({ params }: { params: Promise<{ id: string }> }) => {
                         lg:static
                         lg:bottom-auto
                         ">
-                        Sepete Ekle
+                        {
+                            detailProduct.stock === 0 ? "Üzgünüz ürün stokta kalmadı :(" : "Sepete Ekle"
+                        }
                     </Button>
                 </div>
 
