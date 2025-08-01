6d7113f2fb772d78fbd4de167d7becac3126140d
furkan <furkanktnci@gmail.com>
2025-08-01 12:42:29 +0300

    try lint-staged

--- frontend/app/components/Banner/index.tsx
+++ frontend/app/components/Banner/index.tsx
@@ -2,7 +2,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 
 const images = ['/banner1.jpg', '/banner2.jpg'];
 
